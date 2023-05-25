372c2a3a043a0f8b216ac8df2fe6928c66dedac8
Damien SALEM <d.salem@ecole-ipssi.net>
2023-05-25 16:51:07 +0200

    debugging routes

--- API/routes/items.js
+++ API/routes/items.js
@@ -6,32 +6,42 @@ router
   .get("/items", (req, res) => {
     items.find().then((resultat) => res.json(resultat));
   })
-  .get("/items/:id",(req,res)=>{
-    items.findById(req.params.id)
-    .then(item=>res.json(item))
-    .catch(err=>res.status(404).json({error:"item not found"}))
+  .get("/items/:id", (req, res) => {
+    items
+      .findById(req.params.id)
+      .then((item) => res.json(item))
+      .catch((err) => res.status(404).json({ error: "item not found" }));
   })
-  .post('/items',(req,res)=>{
-    items.create(req.body).then(resultat=>res.json({message:"item added"}))
-    .catch(err=>res.status(400).json({error:"Error in the item creation"}))
+  .post("/items", (req, res) => {
+    items
+      .create(req.body)
+      .then((resultat) => res.json({ message: "item added" }))
+      .catch((err) =>
+        res.status(400).json({ error: "Error in the item creation" })
+      );
   })
-  .put("/items/:id",(req,res)=>{
-    items.findByIdAndUpdate(req.params.id,req.body)
-    .then(resultat=>res.json({message:"update done"}))
-    .catch(err=>res.status(400).json({error:"update failed"}))
+  .put("/items/:id", (req, res) => {
+    items
+      .findByIdAndUpdate(req.params.id, req.body)
+      .then((resultat) => res.json({ message: "update done" }))
+      .catch((err) => res.status(400).json({ error: "update failed" }));
   })
-  .delete("/items/:id",(req,res)=>{
-    items.findByIdAndDelete(req.params.id)
-    .then(resultat=>res.json({message:"item deleted"}))
-    .catch(err=>res.status(404).json({error:"error in deletion"}))
+  .delete("/items/:id", (req, res) => {
+    items
+      .findByIdAndDelete(req.params.id)
+      .then((resultat) => res.json({ message: "item deleted" }))
+      .catch((err) => res.status(404).json({ error: "error in deletion" }));
   })
-  .put("/items/cart",(req,res)=>{
-    req.body.forEach(item => {
-      item_data=items.findById(item.id)
-      item["quantite"]=item_data.quantite-1
-      items.findByIdAndUpdate(item.id,item)
-    .catch(err=>res.status(400).json({error:"update failed"}))
-    }).then(resultat=>res.json({message:"cart buyed"}))
-    .catch(err=>res.status(400).json({error:"update failed"}));
-  })
+  .put("/items/cart", (req, res) => {
+    req.body
+      .forEach((item) => {
+        item_data = items.findById(item.id);
+        item["quantite"] = item_data.quantite - 1;
+        items
+          .findByIdAndUpdate(item.id, item)
+          .catch((err) => res.status(400).json({ error: "update failed" }));
+      })
+      .then((resultat) => res.json({ message: "cart buyed" }))
+      .catch((err) => res.status(400).json({ error: "update failed" }));
+  });
 module.exports = router;
